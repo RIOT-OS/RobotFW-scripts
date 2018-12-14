91cc9510dc3528fddce22967b6316f3257ae6401
smlng <s@mlng.net>
2018-12-14 17:35:26 +0100

    add script to generate combined test output XML
    The scripts searches for xunit.xml files generate by robot framework
    containing testsuite results in board subfolders and combines them into
    a single XML for further processing.

---
+++ hil-xml.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+[ -z "$1" ] && {
+  echo "USAGE: $0 <path/to/robot/results>"
+  return 1
+}
+BASEDIR=$1
+BASEXML=$BASEDIR/robot.xml
+
+# write header to xml
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > $BASEXML
+echo "<?xml-stylesheet type=\"text/xsl\" href=\"https://ci.riot-os.org/static/robot.xsl\"?>" >> $BASEXML
+echo "<result name=\"RIOT HIL\">" >> $BASEXML
+echo "<boards>" >> $BASEXML
+
+BOARDS=$(find ${BASEDIR} -mindepth 1 -maxdepth 1 -type d | sort)
+for b in $BOARDS; do
+  echo " . processing $b"
+  BOARD=$(basename $b)
+  echo " .. BOARD=$BOARD"
+  echo "<board name=\"$BOARD\">" >> $BASEXML
+  TESTSUITES=$(find $b -mindepth 1 -maxdepth 1 -type d)
+  for t in $TESTSUITES; do
+    SUITE=$(basename $t)
+    echo " .. TESTSUITE=$SUITE"
+    [ -f $t/xunit.xml ] && {
+	echo " ... found xunit.xml"
+	tail -n+2 $t/xunit.xml >> $BASEXML
+    }
+  done
+  echo "</board>" >> $BASEXML
+done
+# write footer to xml
+echo "</boards>" >> $BASEXML
+echo "</result>" >> $BASEXML
